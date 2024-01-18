e8c4932fe5391681c8826a4fd076c3e07b2745d0
João Felipe Lima <joaofelipe.work@gmail.com>
2024-01-18 12:00:14 -0300

    feat: Added Tag model

--- project/quiz/models.py
+++ project/quiz/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
+  
+class Tag(models.Model):
+  name = models.CharField(max_length=20)
+  color = models.CharField(max_length=50)
 
 class Quiz(models.Model):
   author_id = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -9,6 +13,7 @@ class Quiz(models.Model):
   start_date = models.DateTimeField()
   expiration_date = models.DateTimeField()
   created_at = models.DateTimeField(auto_now_add=True)
+  tags = models.ManyToManyField(Tag)
 
 class Question(models.Model):
   quiz_id = models.ForeignKey(Quiz, on_delete=models.CASCADE)
