86cf5e9064abdf3f58435f9f07b5039d58cac80f
João Felipe Lima de Oliveira <joao.foliveira@agricultura.gov.br>
2024-01-19 11:58:06 -0300

    feat: Created Tag model and str function

--- project/quiz/admin.py
+++ project/quiz/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Question, Quiz, Tag, UserQuiz, UserResponse, Choice
 
-# Register your models here.
+admin.site.register(Tag)
+admin.site.register(Choice)
+admin.site.register(Quiz)
+admin.site.register(Question)
+admin.site.register(UserQuiz)
+admin.site.register(UserResponse)
